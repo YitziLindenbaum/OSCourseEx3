0878b42efe6823d85bf7fed0168ee368730819cb
yitzchaklindenbaum <yitzchak.lindenbaum@mail.huji.ac.il>
2022-05-04 15:48:49 +0300

    Implemented shuffle (except atomic counter)

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -5,41 +5,57 @@
 #include <iostream>
 #include <algorithm>
 #include <semaphore.h>
+#include <map>
+#include <set>
 #include "SampleClient.cpp"
 #include "Barrier.h"
 
 #define SET_STAGE(atomic_counter, stage) atomic_counter->store((uint64_t)stage << 62)
-#define INC_PROGRESS 1
+#define INC_PROGRESS(atomic_counter) atomic_counter->fetch_add(1)
 #define SET_TOTAL(atomic_counter, new_total) atomic_counter->store(new_total << 31)
 #define INC_TOTAL(atomic_counter, to_add) atomic_counter->fetch_add(to_add << 31)
 
 #define LOAD_STAGE(num) num >> 62
-#define LOAD_TOTAL(num) num & 0x3FFFFFFF80000000
+#define LOAD_TOTAL(num) (num & 0x3FFFFFFF80000000)
 #define LOAD_PROGRESS(num) num & 0x7FFFFFFF
 
-sem_t sem;
+//sem_t sem;
 
-struct
+
+struct ThreadTracker typedef ThreadTracker;
+
+struct ThreadContext
 {
     int tid;
+    const MapReduceClient *client;
+    const InputVec *inputVec;
     IntermediateVec intermediateVec;
     OutputVec outputVec;
     std::atomic<uint64_t> *atomic_counter;
     Barrier *barrier;
+    std::vector<IntermediateVec> *shuffledQueue;
+    ThreadTracker *threadTracker; // will be NULL for all threads other than 0
 } typedef ThreadContext;
 
+struct ThreadTracker {
+    struct ThreadContext *all_contexts;
+    int num_threads;
+} typedef ThreadTracker;
 
-struct StarterPack // todo unite with ThreadContext
-{
-    const MapReduceClient &client;
-    const InputVec &inputVec;
-    ThreadContext &t_context;
-    std::atomic<uint64_t> *atomic_counter;
-} typedef StarterPack;
-
-bool comparePairs(IntermediatePair a, IntermediatePair b) {
+bool comparePairs(const IntermediatePair a, const IntermediatePair b) {
     return *(a.first) < *(b.first);
 }
+
+bool equalPairs(const IntermediatePair a, const IntermediatePair b) {
+    return !comparePairs(a,b) && !comparePairs(b,a);
+}
+
+class IntPairComparator {
+public:
+    bool operator()(const IntermediatePair a, const IntermediatePair b) const {
+        return comparePairs(a, b);
+    }
+};
 
 void* entry_point(void* placeholder);
 
@@ -49,25 +65,33 @@ JobHandle startMapReduceJob(const MapReduceClient &client,
 {
     pthread_t *threads = new pthread_t[multiThreadLevel];
     ThreadContext *t_contexts = new ThreadContext[multiThreadLevel];
-    std::atomic<uint64_t> *atomic_counter = new std::atomic<uint64_t>(0);
+    ThreadTracker *threadTracker = new ThreadTracker();
+    threadTracker->all_contexts = t_contexts;
+    threadTracker->num_threads = multiThreadLevel;
+
+    std::atomic<std::uint64_t> *atomic_counter = new std::atomic<std::uint64_t>(0);
     Barrier *barrier = new Barrier(multiThreadLevel);
-    if (sem_init(&sem, 0, 1) < 0) {
-        //todo print error message
-        exit(1);
-    }
+    auto shuffledQueue = new std::vector<IntermediateVec>();
+
     INC_TOTAL(atomic_counter, inputVec.size());
     SET_STAGE(atomic_counter, MAP_STAGE);
     for (int i = 0; i < multiThreadLevel; ++i) // todo export to function
     {
-        IntermediateVec intermediateVec;
-        t_contexts[i].atomic_counter = atomic_counter;
         t_contexts[i].tid = i;
+        t_contexts[i].client = &client;
+        t_contexts[i].inputVec = &inputVec;
+        t_contexts[i].atomic_counter = atomic_counter;
         t_contexts[i].barrier = barrier;
+        t_contexts[i].shuffledQueue = shuffledQueue;
 
-        StarterPack *starterPack = new StarterPack{client, inputVec, t_contexts[i], atomic_counter};
+        // Give thread 0 access to all threads
+        if (i == 0) {
+            t_contexts[i].threadTracker = threadTracker;
+        }
 
-        pthread_create(threads + i, NULL, entry_point, starterPack);
+        pthread_create(threads + i, NULL, entry_point, t_contexts + i);
     }
+
 
     for (int i = 0; i < multiThreadLevel; ++i)
     {
@@ -100,40 +124,90 @@ JobHandle startMapReduceJob(const MapReduceClient &client,
 void *entry_point(void *placeholder)
 {
 
-    StarterPack *starter_pack = static_cast<StarterPack *>(placeholder);
+    ThreadContext *t_context = static_cast<ThreadContext*>(placeholder);
 
     // ================ MAP STAGE ================
 
-    int num_pairs = starter_pack->inputVec.size();
-    int old_value = LOAD_PROGRESS((starter_pack->atomic_counter)->fetch_add(INC_PROGRESS));
+    int num_pairs = t_context->inputVec->size();
+    int old_value = LOAD_PROGRESS((t_context->atomic_counter)->fetch_add(1));
     while (old_value < num_pairs)
     {
-        K1 *key = starter_pack->inputVec.at(old_value).first;
-        V1 *value = starter_pack->inputVec.at(old_value).second;
-        starter_pack->client.map(key, value, &(starter_pack->t_context));
-        old_value = LOAD_PROGRESS((starter_pack->atomic_counter)->fetch_add(INC_PROGRESS));
+        K1 *key = t_context->inputVec->at(old_value).first;
+        V1 *value = t_context->inputVec->at(old_value).second;
+        t_context->client->map(key, value, t_context);
+        old_value = LOAD_PROGRESS((t_context->atomic_counter)->fetch_add(1));
     }
 
 
-    auto vec_begin = starter_pack->t_context.intermediateVec.begin();
-    auto vec_end = starter_pack->t_context.intermediateVec.end();
+    auto vec_begin = t_context->intermediateVec.begin();
+    auto vec_end = t_context->intermediateVec.end();
     std::sort(vec_begin, vec_end, comparePairs);
 
-    starter_pack->t_context.barrier->barrier();
+    t_context->barrier->barrier();
 
     // ================ SHUFFLE STAGE ================
-
-    if (starter_pack->t_context.tid == 0) {
-        starter_pack->atomic_counter->store(0);
-        SET_STAGE(starter_pack->atomic_counter, SHUFFLE_STAGE);
+    if (t_context->tid == 0) {
+        auto atomic_counter = t_context->atomic_counter;
+        atomic_counter->store(0);
+        //SET_STAGE(atomic_counter, SHUFFLE_STAGE);
         // do shuffle
+
+        std::vector<IntermediateVec> *queue = t_context->shuffledQueue;
+        std::set<IntermediatePair, IntPairComparator> shuffleSet;
+        ThreadContext* all_contexts = t_context->threadTracker->all_contexts;
+        int num_threads = t_context->threadTracker->num_threads;
+        printf("atomic counter before increments: %llx\n",atomic_counter->load());
+
+        for (int i = 0; i < num_threads; ++i) {
+
+            //INC_TOTAL(atomic_counter, all_contexts[i].intermediateVec.size());
+            INC_TOTAL(atomic_counter, 1);
+            printf("atomic counter after increment %d: %llu\n", i, (atomic_counter->load()));
+            if (!(all_contexts[i].intermediateVec.empty()))
+            {
+                shuffleSet.insert(all_contexts[i].intermediateVec.back());
+            }
+        }
+        while (!shuffleSet.empty()) {
+            //todo extract next four lines to function
+            auto it = shuffleSet.end();
+            it--;
+            IntermediatePair to_place = *it;
+            shuffleSet.erase(it);
+            IntermediateVec *keyVec = new IntermediateVec();
+            for (int i = 0; i < num_threads; ++i) {
+                IntermediateVec &curVec = all_contexts[i].intermediateVec;
+                while (!(curVec.empty()) && equalPairs(to_place, curVec.back()))
+                {
+                    keyVec->push_back(curVec.back());
+                    curVec.pop_back();
+                    INC_PROGRESS(atomic_counter);
+                }
+                if (!curVec.empty()) {shuffleSet.insert(curVec.back());}
+            }
+            queue->push_back(*keyVec);
+        }
+        for (const auto &keyVec : *queue) {
+            std::cout << "new vector" << std::endl;
+            std::cout << "[b" ;
+            for (const auto &item : keyVec)
+            {
+                std::cout << " (" << ((const KChar*)(item.first))->c <<
+                          " : "
+                          << ((const VCount*)(item.second))->count <<
+                          ")" << ",";
+            }
+            std::cout << "]" << std::endl;
+        }
+        printf("final total: %llu\n", LOAD_TOTAL(atomic_counter->load()));
+        printf("progress: %llu\n", LOAD_PROGRESS(atomic_counter->load()));
     }
 
-    starter_pack->t_context.barrier->barrier();
+    t_context->barrier->barrier();
 
 
     //TODO change
-    return starter_pack->atomic_counter;
+    return t_context->atomic_counter;
     //===========
 }
 
