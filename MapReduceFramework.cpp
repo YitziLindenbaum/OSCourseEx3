5f33f8c8160741826fd7b61938f6c9ff18025591
yitzchaklindenbaum <yitzchak.lindenbaum@mail.huji.ac.il>
2022-05-05 21:29:45 +0300

    Implemented reduce (without atomic counter)

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -10,6 +10,7 @@
 #include "SampleClient.cpp"
 #include "Barrier.h"
 
+// =================== MACROS ======================
 #define SET_STAGE(atomic_counter, stage) atomic_counter->store((uint64_t)stage << 62)
 #define INC_PROGRESS(atomic_counter) atomic_counter->fetch_add(1)
 #define SET_TOTAL(atomic_counter, new_total) atomic_counter->store((uint64_t)new_total << 31)
@@ -19,18 +20,32 @@
 #define LOAD_TOTAL(num) ((num >> 31) & 0x7FFFFFFF)
 #define LOAD_PROGRESS(num) num & 0x7FFFFFFF
 
-//sem_t sem;
+#define NUM_MUTEXES 2
+#define QUEUE_MUTEX 0
+#define OUTPUT_MUTEX 1
 
-
+// ================= DECLARATIONS ====================
 struct ThreadTracker typedef ThreadTracker;
+struct ThreadContext typedef ThreadContext;
+class IntPairComparator;
+bool comparePairs(const IntermediatePair a, const IntermediatePair b);
+bool equalPairs(const IntermediatePair a, const IntermediatePair b);
+void* entry_point(void* placeholder);
+void threadMap(ThreadContext *t_context);
+IntermediatePair popFromSet(std::set<IntermediatePair, IntPairComparator> &shuffleSet);
+void initShuffle(ThreadContext *t_context, std::set<IntermediatePair, IntPairComparator> &shuffleSet);
+void threadShuffle(ThreadContext *t_context, std::set<IntermediatePair, IntPairComparator> &shuffleSet);
+void threadReduce(ThreadContext *t_context);
 
+// ================= IMPLEMENTATIONS ====================
 struct ThreadContext
 {
     int tid;
     const MapReduceClient *client;
+    pthread_mutex_t* mutexes;
     const InputVec *inputVec;
     IntermediateVec intermediateVec;
-    OutputVec outputVec;
+    OutputVec *outputVec;
     std::atomic<uint64_t> *atomic_counter;
     Barrier *barrier;
     std::vector<IntermediateVec> *shuffleQueue;
@@ -57,143 +72,13 @@ public:
     }
 };
 
-void* entry_point(void* placeholder);
-
-void threadMap(ThreadContext *t_context);
-
-IntermediatePair popFromSet(std::set<IntermediatePair, IntPairComparator> &shuffleSet);
-
-void initShuffle(std::atomic<uint64_t> *atomic_counter, std::set<IntermediatePair, IntPairComparator> &shuffleSet,
-                 const ThreadContext *all_contexts, int num_threads);
-
-void threadShuffle(std::vector<IntermediateVec> *shuffleQueue, std::atomic<uint64_t> *atomic_counter,
-                   std::set<IntermediatePair, IntPairComparator> &shuffleSet, ThreadContext *all_contexts,
-                   int num_threads);
-
-JobHandle startMapReduceJob(const MapReduceClient &client,
-                            const InputVec &inputVec, OutputVec &outputVec,
-                            int multiThreadLevel)
+void threadShuffle(ThreadContext *t_context, std::set<IntermediatePair, IntPairComparator> &shuffleSet)
 {
-    pthread_t *threads = new pthread_t[multiThreadLevel];
-    ThreadContext *t_contexts = new ThreadContext[multiThreadLevel];
-    ThreadTracker *threadTracker = new ThreadTracker();
-    threadTracker->all_contexts = t_contexts;
-    threadTracker->num_threads = multiThreadLevel;
+    auto atomic_counter = t_context->atomic_counter;
+    ThreadContext *all_contexts = t_context->threadTracker->all_contexts;
+    int num_threads = t_context->threadTracker->num_threads;
+    auto shuffleQueue = t_context->shuffleQueue;
 
-    std::atomic<std::uint64_t> *atomic_counter = new std::atomic<std::uint64_t>(0);
-    Barrier *barrier = new Barrier(multiThreadLevel);
-    auto shuffledQueue = new std::vector<IntermediateVec>();
-
-    INC_TOTAL(atomic_counter, inputVec.size());
-    SET_STAGE(atomic_counter, MAP_STAGE);
-    for (int i = 0; i < multiThreadLevel; ++i) // todo export to function
-    {
-        t_contexts[i].tid = i;
-        t_contexts[i].client = &client;
-        t_contexts[i].inputVec = &inputVec;
-        t_contexts[i].atomic_counter = atomic_counter;
-        t_contexts[i].barrier = barrier;
-        t_contexts[i].shuffleQueue = shuffledQueue;
-
-        // Give thread 0 access to all threads
-        if (i == 0) {
-            t_contexts[i].threadTracker = threadTracker;
-        }
-
-        pthread_create(threads + i, NULL, entry_point, t_contexts + i);
-    }
-
-
-    for (int i = 0; i < multiThreadLevel; ++i)
-    {
-        pthread_join(threads[i], NULL);
-    }
-
-
-    //todo remove
-    for (int i = 0; i < multiThreadLevel; ++i)
-    {
-        std::cout << "intermediate_vec for thread " << i << " is: \n [ ";
-        for (int j = 0; j < t_contexts[i].intermediateVec.size(); ++j)
-        {
-
-            std::cout << "(" << ((const KChar *) (t_contexts[i].intermediateVec.at(j).first))->c <<
-                      " : "
-                      << ((const VCount *) (t_contexts[i].intermediateVec.at(j).second))->count <<
-                      ") " << "," << std::endl;
-        }
-        std::cout << "]" << std::endl;
-    }
-    //==========
-
-
-    //TODO change
-    return atomic_counter;
-    //=========
-}
-
-void *entry_point(void *placeholder)
-{
-
-    ThreadContext *t_context = static_cast<ThreadContext*>(placeholder);
-
-    // ================ MAP STAGE ================
-
-    threadMap(t_context);
-
-
-    //            ======= SORT =======
-
-    auto vec_begin = t_context->intermediateVec.begin();
-    auto vec_end = t_context->intermediateVec.end();
-    std::sort(vec_begin, vec_end, comparePairs);
-
-    t_context->barrier->barrier();
-
-
-    // ================ SHUFFLE STAGE ================
-    if (t_context->tid == 0) {
-        auto atomic_counter = t_context->atomic_counter;
-        atomic_counter->store(0);
-        SET_STAGE(atomic_counter, SHUFFLE_STAGE);
-        // do shuffle
-
-        std::set<IntermediatePair, IntPairComparator> shuffleSet;
-        ThreadContext* all_contexts = t_context->threadTracker->all_contexts;
-        int num_threads = t_context->threadTracker->num_threads;
-        // Place "greatest" pairs in each thread's vector in ordered set
-        initShuffle(atomic_counter, shuffleSet, all_contexts, num_threads);
-
-        threadShuffle(t_context->shuffleQueue, atomic_counter, shuffleSet, all_contexts, num_threads);
-        for (const auto &keyVec : *(t_context->shuffleQueue)) {
-            std::cout << "new vector" << std::endl;
-            std::cout << "[" ;
-            for (const auto &item : keyVec)
-            {
-                std::cout << " (" << ((const KChar*)(item.first))->c <<
-                          " : "
-                          << ((const VCount*)(item.second))->count <<
-                          ")" << ",";
-            }
-            std::cout << "]" << std::endl;
-        }
-        // todo remove
-        printf("final total: %llu\n", LOAD_TOTAL(atomic_counter->load()));
-        printf("progress: %llu\n", LOAD_PROGRESS(atomic_counter->load()));
-    }
-
-    t_context->barrier->barrier();
-
-
-    //TODO change
-    return t_context->atomic_counter;
-    //===========
-}
-
-void threadShuffle(std::vector<IntermediateVec> *shuffleQueue, std::atomic<uint64_t> *atomic_counter,
-                   std::set<IntermediatePair, IntPairComparator> &shuffleSet, ThreadContext *all_contexts,
-                   int num_threads)
-{
     while (!shuffleSet.empty()) {
         IntermediatePair toPlace = popFromSet(shuffleSet);
 
@@ -212,9 +97,11 @@ void threadShuffle(std::vector<IntermediateVec> *shuffleQueue, std::atomic<uint6
     }
 }
 
-void initShuffle(std::atomic<uint64_t> *atomic_counter, std::set<IntermediatePair, IntPairComparator> &shuffleSet,
-                 const ThreadContext *all_contexts, int num_threads)
+void initShuffle(ThreadContext *t_context, std::set<IntermediatePair, IntPairComparator> &shuffleSet)
 {
+    auto atomic_counter = t_context->atomic_counter;
+    const ThreadContext *all_contexts = t_context->threadTracker->all_contexts;
+    int num_threads = t_context->threadTracker->num_threads;
     for (int i = 0; i < num_threads; ++i) {
 
         INC_TOTAL(atomic_counter, all_contexts[i].intermediateVec.size());
@@ -247,9 +134,163 @@ void threadMap(ThreadContext *t_context)
     }
 }
 
+void threadReduce(ThreadContext *t_context) {
+    auto shuffleQueue = t_context->shuffleQueue;
+    while (true) {
+        pthread_mutex_lock(t_context->mutexes + QUEUE_MUTEX);
+        if (shuffleQueue->empty()) {
+            pthread_mutex_unlock(t_context->mutexes + QUEUE_MUTEX);
+            break;
+        }
+        t_context->client->reduce(&(shuffleQueue->back()), t_context);
+        t_context->shuffleQueue->pop_back();
+        pthread_mutex_unlock(t_context->mutexes + QUEUE_MUTEX);
+    }
+}
+
+// ================= IMPLEMENTATION OF MapReduceFramework LIBRARY ====================
+
+JobHandle startMapReduceJob(const MapReduceClient &client,
+                            const InputVec &inputVec, OutputVec &outputVec,
+                            int multiThreadLevel)
+{
+    pthread_t *threads = new pthread_t[multiThreadLevel];
+    ThreadContext *t_contexts = new ThreadContext[multiThreadLevel];
+
+    pthread_mutex_t *mutexes = new pthread_mutex_t[NUM_MUTEXES];
+    for (int i = 0; i < NUM_MUTEXES; ++i) {mutexes[i] = PTHREAD_MUTEX_INITIALIZER;}
+
+    ThreadTracker *threadTracker = new ThreadTracker();
+    threadTracker->all_contexts = t_contexts;
+    threadTracker->num_threads = multiThreadLevel;
+
+    std::atomic<std::uint64_t> *atomic_counter = new std::atomic<std::uint64_t>(0);
+    Barrier *barrier = new Barrier(multiThreadLevel);
+    auto shuffledQueue = new std::vector<IntermediateVec>();
+
+    INC_TOTAL(atomic_counter, inputVec.size());
+    SET_STAGE(atomic_counter, MAP_STAGE);
+    for (int i = 0; i < multiThreadLevel; ++i) // todo export to function
+    {
+        t_contexts[i].tid = i;
+        t_contexts[i].client = &client;
+        t_contexts[i].mutexes = mutexes;
+        t_contexts[i].inputVec = &inputVec;
+        t_contexts[i].outputVec = &outputVec;
+        t_contexts[i].atomic_counter = atomic_counter;
+        t_contexts[i].barrier = barrier;
+        t_contexts[i].shuffleQueue = shuffledQueue;
+
+        // Give thread 0 access to all threads
+        if (i == 0) {
+            t_contexts[i].threadTracker = threadTracker;
+        }
+
+        pthread_create(threads + i, NULL, entry_point, t_contexts + i);
+    }
+
+
+    for (int i = 0; i < multiThreadLevel; ++i)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+
+    //todo remove
+    /*for (int i = 0; i < multiThreadLevel; ++i)
+    {
+        std::cout << "intermediate_vec for thread " << i << " is: \n [ ";
+        for (int j = 0; j < t_contexts[i].intermediateVec.size(); ++j)
+        {
+
+            std::cout << "(" << ((const KChar *) (t_contexts[i].intermediateVec.at(j).first))->c <<
+                      " : "
+                      << ((const VCount *) (t_contexts[i].intermediateVec.at(j).second))->count <<
+                      ") " << "," << std::endl;
+        }
+        std::cout << "]" << std::endl;
+    }*/
+    //==========
+
+    std::cout << "[" ;
+    for (const auto &item : outputVec)
+    {
+        std::cout << " (" << ((const KChar*)(item.first))->c <<
+                  " : "
+                  << ((const VCount*)(item.second))->count <<
+                  ")," << std::endl;
+    }
+    std::cout << "]" << std::endl;
+
+    //TODO change
+    return atomic_counter;
+    //=========
+}
+
+
+// (Note: not technically a library function, but placed here for intuitive reasons)
+void *entry_point(void *placeholder)
+{
+
+    ThreadContext *t_context = static_cast<ThreadContext*>(placeholder);
+
+    // ================ MAP STAGE ================
+
+    threadMap(t_context);
+
+    //            ======= SORT =======
+
+    auto vec_begin = t_context->intermediateVec.begin();
+    auto vec_end = t_context->intermediateVec.end();
+    std::sort(vec_begin, vec_end, comparePairs);
+
+    t_context->barrier->barrier();
+
+
+    // ================ SHUFFLE STAGE ================
+    if (t_context->tid == 0) {
+        // set up Shuffle stage
+        t_context->atomic_counter->store(0);
+        SET_STAGE(t_context->atomic_counter, SHUFFLE_STAGE); //TODO check forum about when exactly to change stage
+        // do shuffle
+
+        std::set<IntermediatePair, IntPairComparator> shuffleSet;
+
+        // Place "greatest" pairs in each thread's vector in ordered set
+        initShuffle(t_context, shuffleSet);
+
+        threadShuffle(t_context, shuffleSet);
+
+        /*for (const auto &keyVec : *(t_context->shuffleQueue)) {
+            std::cout << "new vector" << std::endl;
+            std::cout << "[" ;
+            for (const auto &item : keyVec)
+            {
+                std::cout << " (" << ((const KChar*)(item.first))->c <<
+                          " : "
+                          << ((const VCount*)(item.second))->count <<
+                          ")" << ",";
+            }
+            std::cout << "]" << std::endl;
+        }*/
+        // todo remove
+        //printf("final total: %llu\n", LOAD_TOTAL(t_context->atomic_counter->load()));
+        //printf("progress: %llu\n", LOAD_PROGRESS(t_context->atomic_counter->load()));
+    }
+
+    t_context->barrier->barrier();
+
+    // ================ REDUCE STAGE ================
+    threadReduce(t_context);
+
+
+    //TODO change
+    return t_context->atomic_counter;
+    //===========
+}
 
 void emit2(K2 *key, V2 *value, void *context)
-{ // use atomic_counter
+{
     ThreadContext *t_context = static_cast<ThreadContext *>(context);
     t_context->intermediateVec.push_back(IntermediatePair(key, value));
 }
@@ -257,7 +298,9 @@ void emit2(K2 *key, V2 *value, void *context)
 
 void emit3(K3 *key, V3 *value, void *context)
 {
-
+    // is using second mutex here necessary?
+    ThreadContext *t_context = static_cast<ThreadContext *>(context);
+    t_context->outputVec->push_back(OutputPair(key, value));
 }
 
 
